9bbffc75f5b6a6a136894732001e25b79f2ca4d0
lyh <940225627@qq.com>
2021-09-15 21:13:18 +0800

    这一版增加功能英文单词的大小写转换找到敏感词

--- 031902240/main.py
+++ 031902240/main.py
@@ -1,14 +1,26 @@
 #!/usr/bin/env python
 # -*- coding:utf-8 -*-
+# !/usr/bin/env python
+# -*- coding:utf-8 -*-
 import re
 
+path1 = input()
+path2 = input()
+path3 = input()
+total = 0
+file = open(path2, 'rt', encoding='utf-8')
+answer = open(path3, 'wt', encoding='utf-8')
+answer.write('                                                           \n')
 
-def write_to_ans(data_list, num, answer):  # 将一行中找到的敏感词写到文件中 增加
+
+def write_to_ans(data_list, num, line):  # 将每行读出的敏感词答案写到列表中
+    global total
     for item in data_list:
-        answer.write("Line"+num+item+'\n')
+        total = total + 1
+        answer.write("Line:" + str(num) + " <" + item + "> " + item + '\n')
+    return line
 
-
-def start_to_find(path1):  # 将敏感词读出并写入列表
+def start_to_find(path1):  # 将单词文档读入到列表当中
     with open(path1, 'rt', encoding='utf-8') as file:
         word_list = list()
         for line in file.readlines():
@@ -17,46 +29,65 @@ def start_to_find(path1):  # 将敏感词读出并写入列表
     return word_list
 
 
-"""
-寻找敏感词 使用正则表达式 
-首先直接寻找无变形的敏感词
-其次使用正则表达式 首个字符与尾部字符做界限 找到匹配即认为有敏感词
-"""
+def English(line,item,num):
+    if item.islower():  # 判断所有字符都是小写
+        s =item[0].upper()
+        data_list1 = re.findall(s + ".+?" + item[-1], line)  # 大写小写型
+        if data_list1:
+            line = write_to_ans(data_list1, num,line)
+        s = item[-1].upper()
+        data_list2 = re.findall(item[0] + ".?" + s, line)  # 小写大写型
+        if data_list2:
+            line = write_to_ans(data_list2, num,line)
+        data_list3 = re.findall(item[0].upper() + ".?" + item[-1].upper(), line)  #大写大写型
+        if data_list3:
+            line = write_to_ans(data_list3, num, line)
+    return line
 
 
-def serach_sensitive_word(path1, path2, path3):
+
+def instr(item, line_num, line):  # 中间有字符串的情况
+    global total
+    data_list = re.findall(item[0] + ".+?" + item[-1], line)
+    if data_list:
+        for case1 in data_list:
+            total = total + 1
+            num_str = str(line_num)
+            answer.write("Line:" + num_str + " <" + item + "> " + case1 + '\n')
+
+    return line
+
+
+def directly(item,line_num,line):  # 直接删除有对应的单词
+    global total
+    data_list = re.findall(item, line)
+    line = re.sub(item, "", line)  # 找到相匹配的单词并且删
+    if data_list:
+        for case in data_list:
+            total = total + 1
+            num_str = str(line_num)
+            answer.write("Line:" + num_str + " <" + item + "> " + case + '\n')
+    return line
+
+def serach_sensitive_word(path1):  # 寻找敏感词
     word_list = start_to_find(path1)
-    total = 0
-    with open(path2, 'rt', encoding='utf-8') as file, open(path3, 'wt', encoding='utf-8') as answer:
-        answer.write('                                                           \n')
-        line_num = 0
-        for line in file:
-            line_num = line_num + 1
-            for item in word_list:
-                data_list2 = re.findall(item, line)
-                line = re.sub(item, "", line)
-                data_list1 = re.findall(item[0]+".+?"+item[-1], line)
-                if data_list1:
-                    for case1 in data_list1:
-                        total = total + 1
-                        num_str = str(line_num)
-                        answer.write("Line:"+num_str+" <"+item+"> "+case1+'\n')
-                if data_list2:
-                    for case2 in data_list2:
-                        total = total + 1
-                        num_str = str(line_num)
-                        answer.write("Line:" + num_str + " <" + item + "> " + case2 + '\n')
-        answer.seek(0)
-        s1 = str(total)
-        answer.write("Total: " + s1)
+    line_num = 0
+    for line in file:  # 每行的读取
+        line_num = line_num + 1
+        for item in word_list:
+            line = directly(item, line_num, line)
+            line = instr(item, line_num, line)
+            if item.encode('UTF-8').isalpha():  # 判断中英文单词
+                line = English(line, item, line_num)
 
 
 def main():
-    path1 = input()
-    path2 = input()
-    path3 = input()
-    serach_sensitive_word(path1, path2, path3)
-
+    serach_sensitive_word(path1)
+    answer.seek(0)
+    s1 = str(total)
+    answer.write("Total: " + s1)
 
 if __name__ == '__main__':
     main()
+file.close()
+answer.close()
